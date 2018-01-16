c39072c6f520bc478c293d0ee2da5cdf700998d8
Guangtao Zhao <zhaoguangtao@outlook.com>
2018-01-16 16:13:02 +0800

    EventHandler

---
+++ EventSample/EventSample/ThresholdReachedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSample
+{
+    public class ThresholdReachedEventArgs : EventArgs
+    {
+        public int Threshold { get; set; }
+
+        public DateTime TimeReached { get; set; }
+    }
+}
